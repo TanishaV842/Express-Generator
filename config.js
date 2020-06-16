23170abee06a9972dd7f7137febe1df738a57349
T. Vega <tsvega7@gmail.com>
2020-06-16 14:52:10 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '270814317358527',
+        clientSecret: '2397570ac091330118e7e4b1b75f8075'
+    }
 }
